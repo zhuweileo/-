9033af44edbf0833ba6afa421132fdf70a2ea6a3
zhuwei <zhuwei@koolearn-inc.com>
2020-11-06 14:19:16 +0800

    feat: 修改问恰饭群

--- notice.sh
+++ notice.sh
@@ -2,8 +2,7 @@
 
 set -eux
 
-TOKEN="c15308b1ad1d2131606c50bf3e2d5c3b51aac2dc43b8663169ded545c7ce9228"
-data="{\"msgtype\": \"text\",\"text\": {\"content\": \"go to eat right now, 哈哈哈\"}}"
+data="{\"msgtype\": \"text\",\"text\": {\"content\": \"恰饭吗 ？\"}}"
 
 curl -X POST "https://oapi.dingtalk.com/robot/send?access_token=$1" \
 -H "Content-Type: application/json;charset=utf-8" \
