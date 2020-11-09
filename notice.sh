3bc9f5653a54bee3fae09e92438acbfb3cbb9b44
zhuwei <zhuwei@koolearn-inc.com>
2020-11-09 15:02:06 +0800

    fix: post json格式写错了

--- notice.sh
+++ notice.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
-set -eux
-
-data='{"msgtype": "text","text": {"content": "恰饭吗, [鬼脸]？"}},"at": {"isAtAll": true}'
+data='{"msgtype": "text","text": {"content": "恰饭吗, [鬼脸]？"}, "at": {"isAtAll": true}}'
 
 curl -X POST "https://oapi.dingtalk.com/robot/send?access_token=$1" \
 -H "Content-Type: application/json;charset=utf-8" \
